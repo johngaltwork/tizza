fd203623c360548fd57e7ed99a1c2f4738b7f37e
Dmytro Posokhov <dmytro.posokhov@namecheap.com>
2022-09-16 12:28:40 +0300

    init django-migrations commit

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=240)
